c6904a58fd7ccf88c6182dd836bee1744fb90aef
Emir <emirawp@hotmail.com>
2025-01-15 01:54:35 +0300

    default_column bgColor alpha problem fixed

--- src/layouts/AppLayout/pages/context/ImageManagerProvider.jsx
+++ src/layouts/AppLayout/pages/context/ImageManagerProvider.jsx
@@ -22,7 +22,7 @@ function ImageManagerProvider({ children }) {
       fontFamily: "",
       alignment: "left",
       color: { r: 0, g: 0, b: 0, a: 1 },
-      backgroundColor: { r: 255, g: 255, b: 255, a: 0 },
+      backgroundColor: { r: 255, g: 255, b: 255, a: 0.001 },
     }),
     [],
   );
